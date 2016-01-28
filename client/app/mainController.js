1373fbb2164502249c3058b17ca2de787555dc21
KalisaFalzone <kalisa.falzone@gmail.com>
2016-01-27 23:26:40 -0800

    removes bower_components from git

--- client/app/mainController.js
+++ client/app/mainController.js
@@ -1,4 +1,6 @@
 angular.module('playlytics')
 
 .controller('mainController', ['Spotify', '$scope', function(Spotify, $scope){
+  var band = Spotify.search('Pulp', 'artist')
+  .then(function(data) { console.log(data) })
 }])
