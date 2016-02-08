2f4136295d1533b062bda50fcf64ee92a40b69f5
KalisaFalzone <kalisa.falzone@gmail.com>
2016-02-08 01:42:07 -0800

    corrects seconds formatting

--- client/app/analyticsFactory.js
+++ client/app/analyticsFactory.js
@@ -6,6 +6,9 @@ angular.module('playlytics')
     var totalSeconds = arg/1000
     var totalMinutes = totalSeconds/60
     var seconds = Math.floor(totalSeconds%60)
+    if (seconds < 10) {
+      seconds = '0' + seconds + '';
+    }
     var minutes = Math.floor(totalMinutes%60)
     var hours = Math.floor((totalMinutes/60)%60)
     return {seconds: seconds, minutes: minutes, hours: hours}
